1c359f9078c3afc49aed3b70d0296c197a5bc7cc
Edward Marzal <edsanchezmarzal@gmail.com>
2023-03-07 19:34:59 -0500

    some basic variables on python

---
+++ types/01-variables.py
@@ -0,0 +1,12 @@
+name_language = 'python 3'
+base_code = 'hello world'
+print(name_language, base_code)
+
+# Integer (Int)#
+number = 741
+
+# Float (1 decimal)#
+decimal = 7.5
+
+# Boolean (True or False)#
+approved = True
