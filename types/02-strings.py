ecc452f759e7481e2c75eeb04762eced510b2d12
Edward Marzal <edsanchezmarzal@gmail.com>
2023-03-07 20:03:40 -0500

    string bases

---
+++ types/02-strings.py
@@ -0,0 +1,13 @@
+name = 'Arcane'
+description = '''
+A series based on the MOBA league of legends
+'''
+
+print(name, description)
+# len is a function that counts the number of characters in a string#
+print(len(name))
+print(name[0])
+print(name[0:6])
+print(name[2:])
+print(name[:4])
+print(name[:])
