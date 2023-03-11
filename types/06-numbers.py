f4e07af2ecd5cd604fbf46fa6eb071d63e87b8ba
Edward Marzal <edsanchezmarzal@gmail.com>
2023-03-10 22:50:40 -0500

    basic numbers

---
+++ types/06-numbers.py
@@ -0,0 +1,11 @@
+number = 2  # interger
+decimal = 1.2  # float
+imaginary = 2 + 2j  # unreal number
+
+print(1 + 3)
+print(1 - 3)
+print(1 * 3)
+print(1 / 3)
+print(1 // 3)  # no decimals
+print(8 % 3)  # lo que sobra
+print(2 ** 3)  # elevado a
