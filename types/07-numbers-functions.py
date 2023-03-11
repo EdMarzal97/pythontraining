5a83c2d17d22304dbc968e5b3372e47707f0e0e8
Edward Marzal <edsanchezmarzal@gmail.com>
2023-03-10 23:05:21 -0500

    math module

--- types/07-numbers-functions.py
+++ types/07-numbers-functions.py
@@ -8,3 +8,7 @@ print(abs(28))
 
 print(math.ceil(1.22256))  # muestra el numero mas cercano hacia arriba
 print(math.floor(5.894))  # muestra el numero mas cercano hacia abajo
+print(math.isnan(788))  # return true or false asking is this NOT A NUMBER?
+print(math.pow(10, 5))  # elevar un numero a la potencia de
+print(math.sqrt(25))  # sacar raiz cuadrada de
+# SEARCH FOR MATH PYTHON ON GOOGLE AND THERE YOU HAVE DOCUMENTATION ABOUT THE MATH MODULE WITH ALL THE FUNCTIONS
