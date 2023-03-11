3933f265c821634e695d1753908bdbfe9e937bc6
Edward Marzal <edsanchezmarzal@gmail.com>
2023-03-10 22:59:25 -0500

    importing math

--- types/07-numbers-functions.py
+++ types/07-numbers-functions.py
@@ -1,5 +1,10 @@
+import math
+
 print(round(1.8))  # redondea al numero mas cercano
 print(round(1.2))
 print(round(1.5))
 print(abs(-53))  # nos da el valor absoluto
 print(abs(28))
+
+print(math.ceil(1.22256))  # muestra el numero mas cercano hacia arriba
+print(math.floor(5.894))  # muestra el numero mas cercano hacia abajo
